56a84f0b994b720a0aabc5cf1e57ad3039a360e1
AlexanderTarler <saltdev@dev-salts-MBP>
2023-02-15 18:00:17 +0100

    Updates

---
+++ assets/index-89b7438f.js
@@ -0,0 +1,11 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();function c(o){return fetch(o).then(t=>{if(!t.ok)throw new Error(t.statusText);return t.json()})}const l=c("https://lab-mobster-backend-production-3867.up.railway.app/mobs/1").then(({name:o,mobId:t})=>{console.log({name:o,mobId:t})}).catch(o=>{console.log(o)});console.log("MOB: "+l);document.querySelector("#app").innerHTML=`
+<header>
+<h1>Mobster</h1>
+</header>
+<main>
+<div class="member">member</div>
+<div class="member">member</div>
+<div class="member">member</div>
+<div class="member">member</div>
+</main>
+</div>`;
